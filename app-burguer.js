4cdfd407fe43205d3f5f12f5a099a5804a00c804
camilascalzo <camilascalzo@hotmail.com>
2023-08-18 19:03:46 -0300

    update

--- app-burguer.js
+++ app-burguer.js
@@ -1,13 +1,19 @@
 const navSlide = () => {
-    const burguer = document.querySelector(".burguer");
-    // const burguerLines = document.querySelector("#burguer-lines");
-    const nav = document.querySelector (".nav-links");
-    // const closeBurguer = document.querySelector ("#close-burguer");
+   	const burguer = document.querySelector(".burguer");
+    const burguerIcon = document.querySelector("#burguer-icon");
+    const closeIcon = document.querySelector("#close-icon");
+    const nav = document.querySelector(".nav-links");
 
     burguer.addEventListener("click", () => {
         nav.classList.toggle("nav-active");
-    })
-
-}
+				if (nav.classList.contains("nav-active")) {
+					burguerIcon.style.display = "none";
+					closeIcon.style.display = "inline-block";
+			} else {
+					burguerIcon.style.display = "inline-block";
+					closeIcon.style.display = "none";
+			}
+    });
+};
 
 navSlide();
